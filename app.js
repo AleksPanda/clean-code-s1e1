a87d0049c3c388f696be76310ace79648973cb34
AleksPanda <149190602+AleksPanda@users.noreply.github.com>
2025-10-18 23:08:26 +0300

    fix: complete HTML/CSS tag selector replacement to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -9,7 +9,8 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
+// var addButton=document.getElementsByTagName("button")[0];//first button
+var addButton = document.querySelector(".btn");
 var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completedTasks");//completedTasks
 
@@ -82,8 +83,10 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
-    var label=listItem.querySelector("label");
+    // var editInput=listItem.querySelector('input[type=text]');
+    var editInput = listItem.querySelector('.task[type="text"]');
+    // var label=listItem.querySelector("label");
+    var label = listItem.querySelector('.task');
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("editMode");
     //If class of the parent is .editmode
@@ -155,7 +158,8 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
-    var checkBox=taskListItem.querySelector("input[type=checkbox]");
+    // var checkBox=taskListItem.querySelector("input[type=checkbox]");
+    var checkBox = taskListItem.querySelector('.task-checkbox');
     var editButton=taskListItem.querySelector("button.edit");
     var deleteButton=taskListItem.querySelector("button.delete");
 
